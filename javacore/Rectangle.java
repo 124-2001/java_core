8d3f79ccb2a95c3b5f7f69a8d314d31fc0a76bfc
124-2001 <buinam1242001@gmail.com>
2022-08-22 00:36:56 +0700

    firrst

---
+++ javacore/Rectangle.java
@@ -0,0 +1,14 @@
+package vn.tech.javacore;
+
+public class Rectangle {
+    static double a=3;
+    static double b=4;
+
+    public static double tinh_s(){
+        return 2 * a * b;
+    }
+
+    public static double tinh_p(){
+        return (a + b) * 2;
+    }
+}
