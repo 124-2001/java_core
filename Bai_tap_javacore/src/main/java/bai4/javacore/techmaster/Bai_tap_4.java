03e72612a74c9f702546e279f5a9eb2a0fadc75e
124-2001 <buinam1242001@gmail.com>
2022-08-29 18:58:41 +0700

    bai4

--- Bai_tap_javacore/src/main/java/bai4/javacore/techmaster/Bai_tap_4.java
+++ Bai_tap_javacore/src/main/java/bai4/javacore/techmaster/Bai_tap_4.java
@@ -4,7 +4,32 @@ import java.util.Scanner;
 
 public class Bai_tap_4 {
     public static void main(String[] args) {
-        Bai_3();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println();
+            System.out.println();
+            System.out.println("******___________________******");
+            System.out.print("Hay nhap bai toan :");
+            int n = sc.nextInt();
+            while (n > 4) {
+                System.out.print("Khong duoc lon hon 4 hay nhap lai :");
+                n = sc.nextInt();
+            }
+            switch (n) {
+                case 1:
+                    Bai_1();
+                    break;
+                case 2:
+                    Bai_2();
+                    break;
+                case 3:
+                    Bai_3();
+                    break;
+                case 4:
+                    Bai_4();
+                    break;
+            }
+        }
     }
 
     public static void In_Mang(int[] arr){
@@ -25,7 +50,7 @@ public class Bai_tap_4 {
             arrInt[i]=sc.nextInt();
         }
         In_Mang(arrInt);
-        System.out.print("Mang sau khi cong la : ");
+        System.out.println("Mang sau khi cong  ");
         for(int i=0; i<n;i++){
             if(arrInt[i]%2==0){
                 arrInt[i]++;
@@ -63,30 +88,34 @@ public class Bai_tap_4 {
         In_Mang(arrInt);
         //tao 1 mang mới
         int[] new_arrInt = new int[n+1];
-        System.out.print("Mang sau khi chen la: ");
-<<<<<<< HEAD
+        System.out.println("Sau khi chen mang ");
+
+
+
         for(int i=0;i<=n;i++){
-=======
-        for(int i=0;i<n;i++){
->>>>>>> cb0e8c00136716f1587b37ec457d12a071613e86
             if(i==2){
                 new_arrInt[i]=5;
             }
             if(i<2){
                 new_arrInt[i]=arrInt[i];
             }
-<<<<<<< HEAD
             if(i>2 ){
                 new_arrInt[i]=arrInt[i-1];
             }
         }
         In_Mang(new_arrInt);
-=======
-            if(i>2 && i<n){
-                new_arrInt[i+1]=arrInt[i];
+    }
+
+
+    public static void Lay_string_giong_nhau(String[] arr1,String[] arr2){
+        for(int i=0;i< arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    System.out.print(arr1[i]+ "  ");
+                }
             }
         }
-        In_Mang(new_arrInt);
+
     }
 
     public static void Bai_4(){
@@ -96,31 +125,5 @@ public class Bai_tap_4 {
         System.out.print("Cac ky tu giong nhau la : ");
         Lay_string_giong_nhau(array1,array2);
     }
-    public static void Lay_string_giong_nhau(String[] arr1,String[] arr2){
-        for(int i=0;i< arr1.length;i++){
-            for(int j=0;j<arr2.length;j++){
-                if(arr1[i]==arr2[j]){
-                    System.out.print(arr1[i]+ "  ");
-                }
-            }
-        }
->>>>>>> cb0e8c00136716f1587b37ec457d12a071613e86
-    }
 
-    public static void Bai_4(){
-        String[] array1 = {"Python", "JAVA", "PHP", "C#", "C++", "SQL"};
-        String[] array2 = {"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"};
-
-        System.out.print("Cac ky tu giong nhau la : ");
-        Lay_string_giong_nhau(array1,array2);
-    }
-    public static void Lay_string_giong_nhau(String[] arr1,String[] arr2){
-        for(int i=0;i< arr1.length;i++){
-            for(int j=0;j<arr2.length;j++){
-                if(arr1[i]==arr2[j]){
-                    System.out.print(arr1[i]+ "  ");
-                }
-            }
-        }
-    }
 }
